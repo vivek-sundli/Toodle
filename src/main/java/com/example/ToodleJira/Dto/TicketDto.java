f7d88b7d951bc124afebf788fa61bf0657a09cfc
vivek-sundli <viveksundli@gmail.com>
2024-04-11 18:58:37 +0530

    fixing tickets

--- src/main/java/com/example/ToodleJira/Dto/TicketDto.java
+++ src/main/java/com/example/ToodleJira/Dto/TicketDto.java
@@ -15,4 +15,36 @@ public class TicketDto {
     private String Project;
     private String Description;
     private String Priority;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProject() {
+        return Project;
+    }
+
+    public void setProject(String project) {
+        Project = project;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getPriority() {
+        return Priority;
+    }
+
+    public void setPriority(String priority) {
+        Priority = priority;
+    }
 }
